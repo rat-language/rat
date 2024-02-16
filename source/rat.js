1227b2dfbf88efb34a72b136ed9faf2edc37526c
alexcarl98 <alex.alvarez1903@gmail.com>
2024-02-16 00:07:27 -0800

    Minor changes before implementing logic for parser

--- source/rat.js
+++ source/rat.js
@@ -1,5 +1,34 @@
-import * as ohm from "ohm-js";
-import fs from "fs";
+import * as fs from "node:fs/promises"
+import process from "node:process"
+import compile from "./compiler.js"
+import { Program } from "./core.js"
+import stringify from "graph-stringify"
 
-// ...to be continued
-console.log("Hello world")
+const help = `rat compiler
+
+Syntax: rat <filename> <outputType>
+
+Prints to stdout according to <outputType>, which must be one of:
+
+  parsed     a message that the program was matched ok by the grammar
+  analyzed   the statically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+`
+
+async function compileFromFile(filename, outputType) {
+  try {
+    const buffer = await fs.readFile(filename)
+    const compiled = compile(buffer.toString(), outputType)
+    console.log(compiled instanceof Program ? stringify(compiled) : compiled)
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`)
+    process.exitCode = 1
+  }
+}
+// node src/rat.js (filename) parsed
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
+}
