831ca37db2b79a092453cc680769577b6977677e
Ray Toal <rtoal@lmu.edu>
2024-04-03 16:48:58 -0700

    Analyzer stuff

--- src/analyzer.js
+++ src/analyzer.js
@@ -3,68 +3,70 @@
 // internal representation of the program (pretty close to what is usually
 // called the AST). This representation also includes entities from the
 // standard library, as needed.
-import * as core from "./core.js"
+import * as core from "./core.js";
 
-
-const INT = core.Type.INT
-const FLOAT = core.Type.FLOAT
-const STRING = core.Type.STRING
-const BOOLEAN = core.Type.BOOLEAN
-const ANY = core.Type.ANY
-const VOID = core.Type.VOID
-
-
+const INT = core.Type.INT;
+const FLOAT = core.Type.FLOAT;
+const STRING = core.Type.STRING;
+const BOOLEAN = core.Type.BOOLEAN;
+const ANY = core.Type.ANY;
+const VOID = core.Type.VOID;
 
 class Context {
   constructor({ parent, locals = {} }) {
-    this.parent = parent
-    this.locals = new Map(Object.entries(locals))
-    this.withinLoop = false
-    this.function = null
+    this.parent = parent;
+    this.locals = new Map(Object.entries(locals));
+    this.withinLoop = false;
+    this.function = null;
   }
   add(name, entity) {
-    this.locals.set(name, entity)
+    this.locals.set(name, entity);
   }
   lookup(name) {
-    return this.locals.get(name) || this.parent?.lookup(name)
+    return this.locals.get(name) || this.parent?.lookup(name);
   }
   static root() {
-    return new Context({ locals: new Map(Object.entries(core.standardLibrary)) })
+    return new Context({
+      locals: new Map(Object.entries(core.standardLibrary)),
+    });
   }
   newChildContext(props) {
-    return new Context({ ...this, ...props, parent: this, locals: new Map() })
+    return new Context({ ...this, ...props, parent: this, locals: new Map() });
   }
   get(name, expectedType, node) {
-    let entity
+    let entity;
     for (let context = this; context; context = context.parent) {
-      entity = context.locals.get(name)
-      if (entity) break
+      entity = context.locals.get(name);
+      if (entity) break;
     }
-    must(entity, `${name} has not been declared`, node)
+    must(entity, `${name} has not been declared`, node);
     must(
       entity.constructor === expectedType,
       `${name} was expected to be a ${expectedType.name}`,
       node
-    )
-    return entity
+    );
+    return entity;
   }
 }
 
-
 function mustHaveNumericType(e, at) {
-  must([INT, FLOAT].includes(e.type), "Expected a number", at)
+  must([INT, FLOAT].includes(e.type), "Expected a number", at);
 }
 function mustHaveNumericOrStringType(e, at) {
-  must([INT, FLOAT, STRING].includes(e.type), "Expected a number or string", at)
+  must(
+    [INT, FLOAT, STRING].includes(e.type),
+    "Expected a number or string",
+    at
+  );
 }
 function mustHaveBooleanType(e, at) {
-  must(e.type === BOOLEAN, "Expected a boolean", at)
+  must(e.type === BOOLEAN, "Expected a boolean", at);
 }
 function mustHaveIntegerType(e, at) {
-  must(e.type === INT, "Expected an integer", at)
+  must(e.type === INT, "Expected an integer", at);
 }
 function mustBeTheSameType(e1, e2, at) {
-  must(equivalent(e1.type, e2.type), "Operands do not have the same type", at)
+  must(equivalent(e1.type, e2.type), "Operands do not have the same type", at);
 }
 
 function equivalent(t1, t2) {
@@ -81,9 +83,8 @@ function equivalent(t1, t2) {
       equivalent(t1.returnType, t2.returnType) &&
       t1.paramTypes.length === t2.paramTypes.length &&
       t1.paramTypes.every((t, i) => equivalent(t, t2.paramTypes[i])))
-  )
+  );
 }
-
 
 export default function analyze(match) {
   // Track the context manually via a simple variable. The initial context
@@ -91,7 +92,7 @@ export default function analyze(match) {
   // as necessary. When needing to descent into a new scope, create a new
   // context with the current context as its parent. When leaving a scope,
   // reset this variable to the parent context.
-  let context = Context.root()
+  let context = Context.root();
 
   // The single gate for error checking. Pass in a condition that must be true.
   // Use errorLocation to give contextual information about the error that will
@@ -99,104 +100,112 @@ export default function analyze(match) {
   // Ohm's getLineAndColumnMessage will be used to prefix the error message.
   function must(condition, message, errorLocation) {
     if (!condition) {
-      const prefix = errorLocation.at.source.getLineAndColumnMessage()
-      throw new Error(`${prefix}${message}`)
+      const prefix = errorLocation.at.source.getLineAndColumnMessage();
+      throw new Error(`${prefix}${message}`);
     }
   }
 
   function mustNotAlreadyBeDeclared(name, at) {
-    must(!context.locals.has(name), `Identifier ${name} already declared`, at)
+    must(!context.locals.has(name), `Identifier ${name} already declared`, at);
   }
 
   function mustHaveBeenFound(entity, name, at) {
-    must(entity, `Identifier ${name} not declared`, at)
+    must(entity, `Identifier ${name} not declared`, at);
   }
 
   function mustBeAVariable(entity, at) {
     // Bella has two kinds of entities: variables and functions.
-    must(entity?.kind === "Variable", `Functions can not appear here`, at)
+    must(entity?.kind === "Variable", `Functions can not appear here`, at);
   }
 
   function mustBeAFunction(entity, at) {
-    must(entity?.kind === "Function", `${entity.name} is not a function`, at)
+    must(entity?.kind === "Function", `${entity.name} is not a function`, at);
   }
 
   function mustNotBeReadOnly(entity, at) {
-    must(!entity.readOnly, `${entity.name} is read only`, at)
+    must(!entity.readOnly, `${entity.name} is read only`, at);
   }
 
   function mustHaveCorrectArgumentCount(argCount, paramCount, at) {
-    const equalCount = argCount === paramCount
-    must(equalCount, `${paramCount} argument(s) required but ${argCount} passed`, at)
+    const equalCount = argCount === paramCount;
+    must(
+      equalCount,
+      `${paramCount} argument(s) required but ${argCount} passed`,
+      at
+    );
   }
 
   const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
     Program(statements) {
-      return core.program(statements.children.map(s => s.rep()))
+      return core.program(statements.children.map((s) => s.rep()));
     },
 
     //==================== (VALID STATEMENTS) ====================//
     Stmt_print(_print, _lparen, exp, _rparen, _semicolon) {
-      return core.printStatement(exp.rep())
+      return core.printStatement(exp.rep());
     },
 
     //Variable Declaration
     Stmt_vardec(modifier, id, _colon, type, _eq, exp, _semicolon) {
       // TODO: Need to do something else with the 'type'
-      const initializer = exp.rep()
-      const readOnly = modifier.sourceString === "const"
-      const variable = core.variable(id.sourceString, readOnly, initializer.type)
-      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
+      const initializer = exp.rep();
+      const readOnly = modifier.sourceString === "const";
+      const variable = core.variable(
+        id.sourceString,
+        readOnly,
+        initializer.type
+      );
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
       // TODO: Add type checking
       // exp must be of type 'type'
-      context.add(id.sourceString, variable)
+      context.add(id.sourceString, variable);
       // Need to make sure this we can both read and write to this variable
 
-      return core.variableDeclaration(variable, initializer)
+      return core.variableDeclaration(variable, initializer);
     },
 
     //Assignment
-    Stmt_assign(id, _eq, exp, _semicolon) {
-      const target = id.rep()
-      mustNotBeReadOnly(target, { at: id })
-      return core.assignment(target, exp.rep())
+    Stmt_assign(id, ops, _eq, exp, _semicolon) {
+      const target = id.rep();
+      mustNotBeReadOnly(target, { at: id });
+      return core.assignment(target, exp.rep());
     },
 
     //While
     Stmt_while(_while, exp, block) {
-      return core.whileStatement(exp.rep(), block.rep())
+      return core.whileStatement(exp.rep(), block.rep());
     },
 
     //For
-    Stmt_for(_for, id, _in, iterable, block) {
-      return core.forStatement(id.sourceString, iterable.rep(), block.rep())
+    ForStmt(_for, id, _in, iterable, block) {
+      return core.forStatement(id.sourceString, iterable.rep(), block.rep());
     },
 
     //Call
-    Stmt_call(id, args, _semicolon) {
-      return core.call(id.sourceString, args.rep())
+    Stmt_call(call, _semicolon) {
+      return core.callStatement(call.rep());
     },
 
     //If
-    Stmt_if(_if, exp, block, _else, elseBlock) {
+    IfStmt(_if, exp, block, _else, elseBlock) {
       //TODO
     },
 
     //Pass
-    Stmt_pass(_pass, _semicolon) {
-      return core.passStatement()
+    PassStmt(_pass, _semicolon) {
+      return core.passStatement();
     },
 
     //Break
     Stmt_break(breakKeyword, _semicolon) {
-      mustBeInLoop(context, { at: breakKeyword })
-      return core.breakStatement()
+      mustBeInLoop(context, { at: breakKeyword });
+      return core.breakStatement();
     },
 
     //Return
-    Stmt_return(_return, exp, _semicolon) {
-      mustBeAFunction(context, { at: _return })
-      return core.returnStatement(exp.rep())
+    RtrnStmt(_return, exp, _semicolon) {
+      mustBeAFunction(context, { at: _return });
+      return core.returnStatement(exp.rep());
     },
 
     // //Try
@@ -206,39 +215,39 @@ export default function analyze(match) {
 
     //Function Declaration
     Stmt_FuncDecl(type, id, parameters, body) {
-      const fun = new core.Function(id.sourceString, params.length, true)
-      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
-      context.add(id.sourceString, fun)
+      const fun = new core.Function(id.sourceString, params.length, true);
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
+      context.add(id.sourceString, fun);
       // Add the function to the context before analyzing the body, because
       // we want to allow functions to be recursive
 
-      context = context.newChildContext({ inLoop: false, function: fun })
-      const params = params.rep()
+      context = context.newChildContext({ inLoop: false, function: fun });
+      const params = params.rep();
 
-      const paramTypes = params.map(p => p.type)
-      const returnType = type.children?.[0]?.rep() ?? VOID
-      fun.type = new core.FunctionType(paramTypes, returnType)
+      const paramTypes = params.map((p) => p.type);
+      const returnType = type.children?.[0]?.rep() ?? VOID;
+      fun.type = new core.FunctionType(paramTypes, returnType);
 
-      const bodyRep = body.rep()
+      const bodyRep = body.rep();
 
-      context = context.parent
-      return new core.functionDeclaration(fun, params, bodyRep)
+      context = context.parent;
+      return new core.functionDeclaration(fun, params, bodyRep);
     },
 
     Params(_open, paramList, _close) {
       // Returns a list of variable nodes
-      return paramList.asIteration().children.map(p => p.rep())
+      return paramList.asIteration().children.map((p) => p.rep());
     },
 
     Param(id, _colon, type) {
-      const param = core.variable(id.sourceString, false, type.rep())
-      mustNotAlreadyBeDeclared(param.name, { at: id })
-      context.add(param.name, param)
-      return param
+      const param = core.variable(id.sourceString, false, type.rep());
+      mustNotAlreadyBeDeclared(param.name, { at: id });
+      context.add(param.name, param);
+      return param;
     },
 
     Block(_open, statements, _close) {
-      return statements.children.map(s => s.rep())
+      return statements.children.map((s) => s.rep());
     },
 
     /*
@@ -246,15 +255,14 @@ IDK how to properly name these statement functions,
 I'm thinking that I might need to go back and re-write the ohm grammars, for now, I'm naming them the variable names...
 */
 
-
     //==================== (EXPRESSIONS) ====================//
     Exp_unwrap(exp1, op, exp2) {
-      return core.binary(op.sourceString, exp1.rep(), exp2.rep())
+      return core.binary(op.sourceString, exp1.rep(), exp2.rep());
     },
 
     Exp_unary(op, exp) {
       // either for negation or for boolean not
-      return core.unary(op.sourceString, exp.rep())
+      return core.unary(op.sourceString, exp.rep());
     },
 
     // IDK HOW TO DO THIS ONE YET!!!
@@ -263,50 +271,52 @@ I'm thinking that I might need to go back and re-write the ohm grammars, for now
     // }
 
     Exp0_logicalor(exp1, _or, exp2) {
-      return core.binary("||", exp1.rep(), exp2.rep())
+      return core.binary("||", exp1.rep(), exp2.rep());
     },
 
     Disjunct_logicaland(exp1, _and, exp2) {
-      return core.binary("&&", exp1.rep(), exp2.rep())
+      return core.binary("&&", exp1.rep(), exp2.rep());
     },
 
     Conjunct_comparative(exp1, op, exp2) {
-      // "<=" | "<" | "==" | "!=" | ">=" | ">" 
-      return core.binary(op.sourceString, exp1.rep(), exp2.rep())
+      // "<=" | "<" | "==" | "!=" | ">=" | ">"
+      return core.binary(op.sourceString, exp1.rep(), exp2.rep());
     },
 
     Comp_additive(exp1, op, exp2) {
       // plus or minus
-      return core.binary(op.sourceString, exp1.rep(), exp2.rep())
+      return core.binary(op.sourceString, exp1.rep(), exp2.rep());
     },
 
     Term_multiplicative(exp1, op, exp2) {
       // times, divide, or modulo
-      return core.binary(op.sourceString, exp1.rep(), exp2.rep())
+      return core.binary(op.sourceString, exp1.rep(), exp2.rep());
     },
 
     Factor_exponent(exp1, _op, exp2) {
       // exponentiation
-      return core.binary("**", exp1.rep(), exp2.rep())
+      return core.binary("**", exp1.rep(), exp2.rep());
     },
 
     // IDK HOW TO DO THIS ONE YET!!!
     // Primary_wrapped(_open, exp, _close) { return exp.rep() },
     // Primary_lookup(id) {},
 
-    Primary_call(id, _open, expList, _close) {
+    Call(id, _open, expList, _close) {
       // ids used in calls must have already been declared and must be
       // bound to function entities, not to variable entities.
-      const callee = context.lookup(id.sourceString)
-      mustHaveBeenFound(callee, id.sourceString, { at: id })
-      mustBeAFunction(callee, { at: id })
-      const args = expList.asIteration().children.map(arg => arg.rep())
-      mustHaveCorrectArgumentCount(args.length, callee.paramCount, { at: id })
-      return core.call(callee, args)
+      const callee = context.lookup(id.sourceString);
+      mustHaveBeenFound(callee, id.sourceString, { at: id });
+      mustBeAFunction(callee, { at: id });
+      const args = expList.asIteration().children.map((arg) => arg.rep());
+      mustHaveCorrectArgumentCount(args.length, callee.paramCount, { at: id });
+      return core.call(callee, args);
     },
 
     ArrayLit(_open, expList, _close) {
-      return core.arrayLiteral(expList.asIteration().children.map(exp => exp.rep()))
+      return core.arrayLiteral(
+        expList.asIteration().children.map((exp) => exp.rep())
+      );
     },
 
     // THIS IS DEFINITELY WRONG BUT WE'LL FIX IT LATER
@@ -315,46 +325,45 @@ I'm thinking that I might need to go back and re-write the ohm grammars, for now
     // },
     //-------------------- (TYPES) -------------------//
     TypeConv(type, _open, exp, _close) {
-      return core.typeConversion(type.sourceString, exp.rep())
+      return core.typeConversion(type.sourceString, exp.rep());
     },
 
     Parens(_open, exp, _close) {
-      return exp.rep()
+      return exp.rep();
     },
 
     Primary_id(id) {
       // ids used in expressions must have been already declared and must
       // be bound to variable entities, not function entities.
-      const entity = context.lookup(id.sourceString)
-      mustHaveBeenFound(entity, id.sourceString, { at: id })
-      mustBeAVariable(entity, { at: id })
-      return entity
+      const entity = context.lookup(id.sourceString);
+      mustHaveBeenFound(entity, id.sourceString, { at: id });
+      mustBeAVariable(entity, { at: id });
+      return entity;
     },
 
     true(_) {
-      return true
+      return true;
     },
 
     false(_) {
-      return false
+      return false;
     },
 
     intlit(_digits) {
       // ints will be represented as plain JS bigints
-      return BigInt(this.sourceString)
+      return BigInt(this.sourceString);
     },
 
     floatlit(_whole, _point, _fraction, _e, _sign, _exponent) {
       // floats will be represented as plain JS numbers
-      return Number(this.sourceString)
+      return Number(this.sourceString);
     },
 
     stringlit(_openQuote, _chars, _closeQuote) {
       // strings will be represented as plain JS strings, including
       // the quotation marks
-      return this.sourceString
+      return this.sourceString;
     },
-
-  })
-  return builder(match).rep()
+  });
+  return builder(match).rep();
 }
--- src/core.js
+++ src/core.js
@@ -1,15 +1,30 @@
-
 export function program(statements) {
-  return { kind: "Program", statements }
+  return { kind: "Program", statements };
+}
+
+export class Type {
+  // Type of all basic type int, float, string, etc. and superclass of others
+  static BOOLEAN = new Type("bool");
+  static INT = new Type("int");
+  static FLOAT = new Type("float");
+  static STRING = new Type("str");
+  static VOID = new Type("void");
+  static ANY = new Type("any");
+  constructor(description) {
+    // The description is a convenient way to view the type. For basic
+    // types or structs, it will just be the names. For arrays, you will
+    // see "[T]". For optionals, "T?". For functions "(T1,...Tn)->T0".
+    Object.assign(this, { description });
+  }
 }
 
 //==================== (VALID STATEMENTS) ====================//
 export function printStatement(argument) {
-  return { kind: "PrintStatement", argument }
+  return { kind: "PrintStatement", argument };
 }
 
 export function variableDeclaration(variable, initializer) {
-  return { kind: "VariableDeclaration", variable, initializer } 
+  return { kind: "VariableDeclaration", variable, initializer };
 }
 /*
 var x:int = 42;
@@ -29,22 +44,22 @@ var x:int = 42;
 */
 
 export function variable(name, readOnly, type) {
-  return { kind: "Variable", name, readOnly, type }
+  return { kind: "Variable", name, readOnly, type };
 }
 
 export function constantDeclaration(variable, initializer) {
   // temporary
-  return { kind: "ConstantDeclaration", variable, initializer }
+  return { kind: "ConstantDeclaration", variable, initializer };
 }
 
-// Must be changed 
+// Must be changed
 export function assignment(target, source) {
-  return { kind: "Assignment", target, source }
+  return { kind: "Assignment", target, source };
 }
 
 // NOTE: This can stay the same
 export function whileStatement(test, body) {
-  return { kind: "WhileStatement", test, body }
+  return { kind: "WhileStatement", test, body };
 }
 
 // ***************** (NEW) ***************** //
@@ -53,43 +68,47 @@ export function whileStatement(test, body) {
 // }
 
 export function forStatement(init, test, update, body) {
-  return { kind: "WhileStatement", test, body }
+  return { kind: "WhileStatement", test, body };
 }
 
-// NOTE: This can stay the same
+export function callStatement(call) {
+  return { kind: "CallStatement", call };
+}
+
 export function call(callee, args) {
-  return { kind: "Call", callee, args }
+  // This can appear within a CallStatement or as a primary expression
+  return { kind: "Call", callee, args };
 }
 
 // Note: This can stay the same
 export function conditional(test, consequent, alternate) {
-  return { kind: "Conditional", test, consequent, alternate }
+  return { kind: "Conditional", test, consequent, alternate };
 }
 
 export function passStatement(_pass, _semicolon) {
-  return { kind: "PassStatement" }
+  return { kind: "PassStatement" };
 }
 
 export function breakStatement(_break, _semicolon) {
-  return { kind: "BreakStatement" }
+  return { kind: "BreakStatement" };
 }
 
 export function returnStatement(expression) {
-  return { kind: "ReturnStatement", expression }
+  return { kind: "ReturnStatement", expression };
 }
 
 // ***************** (NEW) ***************** //
 export function tryStatement(body, catchClause, finallyClause) {
-  return { kind: "TryStatement", body, catchClause, finallyClause }
+  return { kind: "TryStatement", body, catchClause, finallyClause };
 }
 
-// Must be changed 
+// Must be changed
 export function functionDeclaration(fun, params, body) {
-  return { kind: "FunctionDeclaration", fun, params, body }
+  return { kind: "FunctionDeclaration", fun, params, body };
 }
 
 export function fun(name, paramCount) {
-  return { kind: "Function", name, paramCount }
+  return { kind: "Function", name, paramCount };
 }
 
 // ***************** (NEW) ***************** //
@@ -98,52 +117,49 @@ export function importStatement() {}
 // ***************** (NEW) ***************** //
 export function importFromStatement() {}
 
-
 //==========================( EXPRESSIONS )================================//
 
-
 export function binary(op, left, right) {
-  return { kind: "BinaryExpression", op, left, right }
+  return { kind: "BinaryExpression", op, left, right };
 }
 
 export function unary(op, operand) {
-  return { kind: "UnaryExpression", op, operand }
+  return { kind: "UnaryExpression", op, operand };
 }
 
 //------------------------------- (TYPES) ---------------------------------//
 export function optionalType(baseType) {
-  return { kind: "OptionalType", baseType }
+  return { kind: "OptionalType", baseType };
 }
 
 export function promiseType(baseType) {
-  return { kind: "PromiseType", baseType }
+  return { kind: "PromiseType", baseType };
 }
 
 export function arrayType(baseType) {
-  return { kind: "ArrayType", baseType }
+  return { kind: "ArrayType", baseType };
 }
 
 // ***************** (NEW) ***************** //
 export function dictionaryType(keyType, valueType) {
-  return { kind: "DictionaryType", keyType, valueType } 
+  return { kind: "DictionaryType", keyType, valueType };
 }
 
 export function iterableType(type) {
-  return { kind: "IterableType", type }
+  return { kind: "IterableType", type };
 }
 
-export const boolType = { kind: "BoolType" }
-export const intType = { kind: "IntType" }
-export const floatType = { kind: "FloatType" }
-export const stringType = { kind: "StringType" }
-export const voidType = { kind: "VoidType" }
-export const anyType = { kind: "AnyType" }
+export const boolType = { kind: "BoolType" };
+export const intType = { kind: "IntType" };
+export const floatType = { kind: "FloatType" };
+export const stringType = { kind: "StringType" };
+export const voidType = { kind: "VoidType" };
+export const anyType = { kind: "AnyType" };
 
-// These local constants are used to simplify the standard library definitions.
-const floatToFloatType = functionType([floatType], floatType)
-const floatFloatToFloatType = functionType([floatType, floatType], floatType)
-const stringToIntsType = functionType([stringType], arrayType(intType))
-const anyToVoidType = functionType([anyType], voidType)
+String.prototype.type = Type.STRING;
+Number.prototype.type = Type.FLOAT;
+BigInt.prototype.type = Type.INT;
+Boolean.prototype.type = Type.BOOLEAN;
 
 export const standardLibrary = Object.freeze({
   int: Type.INT,
@@ -152,74 +168,11 @@ export const standardLibrary = Object.freeze({
   str: Type.STRING,
   void: Type.VOID,
   any: Type.ANY,
-  π: new Variable("π", true, Type.FLOAT),
-  sin: new Function("sin", 1, true),
-  sqrt: new Function("sqrt", 1, true),
-  cos: new Function("cos", 1, true),
-  exp: new Function("exp", 1, true),
-  ln: new Function("ln", 1, true),
-  hypot: new Function("hypot", 2, true),
-})
-
-String.prototype.type = Type.STRING
-Number.prototype.type = Type.FLOAT
-BigInt.prototype.type = Type.INT
-Boolean.prototype.type = Type.BOOLEAN
-
-
-export class Type {
-  // Type of all basic type int, float, string, etc. and superclass of others
-  static BOOLEAN = new Type("bool")
-  static INT = new Type("int")
-  static FLOAT = new Type("float")
-  static STRING = new Type("str")
-  static VOID = new Type("void")
-  static ANY = new Type("any")
-  constructor(description) {
-    // The description is a convenient way to view the type. For basic
-    // types or structs, it will just be the names. For arrays, you will
-    // see "[T]". For optionals, "T?". For functions "(T1,...Tn)->T0".
-    Object.assign(this, { description })
-  }
-}
-
-
-// Program.prototype[util.inspect.custom] = function () {
-//   const tags = new Map()
-
-//   // Attach a unique integer tag to every node
-//   function tag(node) {
-//     if (tags.has(node) || typeof node !== "object" || node === null) return
-//     if (node.constructor === Token) {
-//       // Tokens are not tagged themselves, but their values might be
-//       tag(node?.value)
-//     } else {
-//       // Non-tokens are tagged
-//       tags.set(node, tags.size + 1)
-//       for (const child of Object.values(node)) {
-//         Array.isArray(child) ? child.forEach(tag) : tag(child)
-//       }
-//     }
-//   }
-
-//   function* lines() {
-//     function view(e) {
-//       if (tags.has(e)) return `#${tags.get(e)}`
-//       if (e?.constructor === Token) {
-//         return `(${e.category},"${e.lexeme}"${
-//           e.value ? "," + view(e.value) : ""
-//         })`
-//       }
-//       if (Array.isArray(e)) return `[${e.map(view)}]`
-//       return util.inspect(e)
-//     }
-//     for (let [node, id] of [...tags.entries()].sort((a, b) => a[1] - b[1])) {
-//       let type = node.constructor.name
-//       let props = Object.entries(node).map(([k, v]) => `${k}=${view(v)}`)
-//       yield `${String(id).padStart(4, " ")} | ${type} ${props.join(" ")}`
-//     }
-//   }
-
-//   tag(this)
-//   return [...lines()].join("\n")
-// }
+  // π: new Variable("π", true, Type.FLOAT),
+  // sin: new Function("sin", 1, true),
+  // sqrt: new Function("sqrt", 1, true),
+  // cos: new Function("cos", 1, true),
+  // exp: new Function("exp", 1, true),
+  // ln: new Function("ln", 1, true),
+  // hypot: new Function("hypot", 2, true),
+});
