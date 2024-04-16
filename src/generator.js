08db46dc4dbb3f582ba6f8a1d03d4767985dea87
alexcarl98 <alex.alvarez1903@gmail.com>
2024-04-15 17:17:32 -0700

    Adding optimizer

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,194 @@
-export default function generate() {
-  throw new Error("Not yet implemented")
+// TODO: Change from carlos to Rat
+
+// The code generator exports a single function, generate(program), which
+// accepts a program representation and returns the JavaScript translation
+// as a string.
+
+import { voidType, standardLibrary } from "./core.js"
+
+export default function generate(program) {
+  // When generating code for statements, we'll accumulate the lines of
+  // the target code here. When we finish generating, we'll join the lines
+  // with newlines and return the result.
+  const output = []
+
+  const standardFunctions = new Map([
+    [standardLibrary.print, x => `console.log(${x})`],
+    [standardLibrary.sin, x => `Math.sin(${x})`],
+    [standardLibrary.cos, x => `Math.cos(${x})`],
+    [standardLibrary.exp, x => `Math.exp(${x})`],
+    [standardLibrary.ln, x => `Math.log(${x})`],
+    [standardLibrary.hypot, ([x, y]) => `Math.hypot(${x},${y})`],
+    [standardLibrary.bytes, s => `[...Buffer.from(${s}, "utf8")]`],
+    [standardLibrary.codepoints, s => `[...(${s})].map(s=>s.codePointAt(0))`],
+  ])
+
+  // Variable and function names in JS will be suffixed with _1, _2, _3,
+  // etc. This is because "switch", for example, is a legal name in Carlos,
+  // but not in JS. So, the Carlos variable "switch" must become something
+  // like "switch_1". We handle this by mapping each name to its suffix.
+  const targetName = (mapping => {
+    return entity => {
+      if (!mapping.has(entity)) {
+        mapping.set(entity, mapping.size + 1)
+      }
+      return `${entity.name}_${mapping.get(entity)}`
+    }
+  })(new Map())
+
+  const gen = node => generators?.[node?.kind]?.(node) ?? node
+
+  const generators = {
+    // Key idea: when generating an expression, just return the JS string; when
+    // generating a statement, write lines of translated JS to the output array.
+    Program(p) {
+      p.statements.forEach(gen)
+    },
+    
+    VariableDeclaration(d) {
+      // We don't care about const vs. let in the generated code! The analyzer has
+      // already checked that we never updated a const, so let is always fine.
+      output.push(`let ${gen(d.variable)} = ${gen(d.initializer)};`)
+    },
+    TypeDeclaration(d) {
+      // The only type declaration in Carlos is the struct! Becomes a JS class.
+      output.push(`class ${gen(d.type)} {`)
+      output.push(`constructor(${d.type.fields.map(gen).join(",")}) {`)
+      for (let field of d.type.fields) {
+        output.push(`this[${JSON.stringify(gen(field))}] = ${gen(field)};`)
+      }
+      output.push("}")
+      output.push("}")
+    },
+    StructType(t) {
+      return targetName(t)
+    },
+    Field(f) {
+      return targetName(f)
+    },
+    FunctionDeclaration(d) {
+      output.push(`function ${gen(d.fun)}(${d.params.map(gen).join(", ")}) {`)
+      d.body.forEach(gen)
+      output.push("}")
+    },
+    Variable(v) {
+      // Standard library constants just get special treatment
+      if (v === standardLibrary.π) return "Math.PI"
+      return targetName(v)
+    },
+    Function(f) {
+      return targetName(f)
+    },
+    Increment(s) {
+      output.push(`${gen(s.variable)}++;`)
+    },
+    Decrement(s) {
+      output.push(`${gen(s.variable)}--;`)
+    },
+    Assignment(s) {
+      output.push(`${gen(s.target)} = ${gen(s.source)};`)
+    },
+    BreakStatement(s) {
+      output.push("break;")
+    },
+    ReturnStatement(s) {
+      output.push(`return ${gen(s.expression)};`)
+    },
+    ShortReturnStatement(s) {
+      output.push("return;")
+    },
+    IfStatement(s) {
+      output.push(`if (${gen(s.test)}) {`)
+      s.consequent.forEach(gen)
+      if (s.alternate?.kind?.endsWith?.("IfStatement")) {
+        output.push("} else")
+        gen(s.alternate)
+      } else {
+        output.push("} else {")
+        s.alternate.forEach(gen)
+        output.push("}")
+      }
+    },
+    ShortIfStatement(s) {
+      output.push(`if (${gen(s.test)}) {`)
+      s.consequent.forEach(gen)
+      output.push("}")
+    },
+    WhileStatement(s) {
+      output.push(`while (${gen(s.test)}) {`)
+      s.body.forEach(gen)
+      output.push("}")
+    },
+    RepeatStatement(s) {
+      // JS can only repeat n times if you give it a counter variable!
+      const i = targetName({ name: "i" })
+      output.push(`for (let ${i} = 0; ${i} < ${gen(s.count)}; ${i}++) {`)
+      s.body.forEach(gen)
+      output.push("}")
+    },
+    ForRangeStatement(s) {
+      const i = targetName(s.iterator)
+      const op = s.op === "..." ? "<=" : "<"
+      output.push(`for (let ${i} = ${gen(s.low)}; ${i} ${op} ${gen(s.high)}; ${i}++) {`)
+      s.body.forEach(gen)
+      output.push("}")
+    },
+    ForStatement(s) {
+      output.push(`for (let ${gen(s.iterator)} of ${gen(s.collection)}) {`)
+      s.body.forEach(gen)
+      output.push("}")
+    },
+    Conditional(e) {
+      return `((${gen(e.test)}) ? (${gen(e.consequent)}) : (${gen(e.alternate)}))`
+    },
+    BinaryExpression(e) {
+      const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op
+      return `(${gen(e.left)} ${op} ${gen(e.right)})`
+    },
+    UnaryExpression(e) {
+      const operand = gen(e.operand)
+      if (e.op === "some") {
+        return operand
+      } else if (e.op === "#") {
+        return `${operand}.length`
+      } else if (e.op === "random") {
+        return `((a=>a[~~(Math.random()*a.length)])(${operand}))`
+      }
+      return `${e.op}(${operand})`
+    },
+    EmptyOptional(e) {
+      return "undefined"
+    },
+    SubscriptExpression(e) {
+      return `${gen(e.array)}[${gen(e.index)}]`
+    },
+    ArrayExpression(e) {
+      return `[${e.elements.map(gen).join(",")}]`
+    },
+    EmptyArray(e) {
+      return "[]"
+    },
+    MemberExpression(e) {
+      const object = gen(e.object)
+      const field = JSON.stringify(gen(e.field))
+      const chain = e.op === "." ? "" : e.op
+      return `(${object}${chain}[${field}])`
+    },
+    FunctionCall(c) {
+      const targetCode = standardFunctions.has(c.callee)
+        ? standardFunctions.get(c.callee)(c.args.map(gen))
+        : `${gen(c.callee)}(${c.args.map(gen).join(", ")})`
+      // Calls in expressions vs in statements are handled differently
+      if (c.callee.type.returnType !== voidType) {
+        return targetCode
+      }
+      output.push(`${targetCode};`)
+    },
+    ConstructorCall(c) {
+      return `new ${gen(c.callee)}(${c.args.map(gen).join(", ")})`
+    },
+  }
+
+  gen(program)
+  return output.join("\n")
 }
