757b45344cd83bbd65085d07fc6bd12d9c88b843
rilozos <rkehoe@lion.lmu.edu>
2024-04-23 13:54:25 -0700

    added generator tests

--- test/generator.test.js
+++ test/generator.test.js
@@ -10,5 +10,52 @@ function dedent(s) {
 
 const fixtures = [
     {
+        name: "while loop",
+        source: `
+        var i:int = 0;
+        while (i <= 10){
+          print(i);
+          i += 1;
+        }
+        `,
+        expected: dedent`
+        let i = 0;
+        while (i <= 10) {
+          console.log(i);
+          i += 1;
+        }
+        `
+    },
+    {
+        name: "call",
+        source: `
+        int sqr(x: int) {return (x * x);}
+        print(sqr(3) + 1);
+        `,
+        expected: dedent`
+        function sqr(x) {
+          return x * x;
+        }
+        console.log(sqr(3) + 1);
+        `
+    },
+    {
+        name: "for loop",
+        source: `
+        for j in 1...10 {
+            print(j);
+            if j > 8 {
+              break;
+            }
+          }
+        `,
+        expected: dedent`
+        for (let j = 1; j <= 10; j++) {
+          console.log(j);
+          if (j > 8) {
+            break;
+          }
+        }
+        `
     }
 ]
